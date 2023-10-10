4979d817982e16d11e1c0699896b42fb128610a9
Todor Dimitrov <to666ko90@abv.bg>
2023-10-11 01:51:14 +0300

    Few more features.

--- src/main/java/org/softuni/mobilele/model/entity/UserEntity.java
+++ src/main/java/org/softuni/mobilele/model/entity/UserEntity.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 @Table(name = "users")
 public class UserEntity extends BaseEntity{
 
+    @Column(unique = true)
     private String email;
 
     private String password;
