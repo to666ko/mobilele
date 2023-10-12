9a12a31f84b735eec340b02f73d0016b64561a6b
Todor Dimitrov <to666ko90@abv.bg>
2023-10-12 23:39:14 +0300

    Bug fix

--- src/main/java/org/softuni/mobilele/web/OfferController.java
+++ src/main/java/org/softuni/mobilele/web/OfferController.java
@@ -42,7 +42,7 @@ public class OfferController {
 
 
     @GetMapping("/{uuid}/details")
-    public String details(@PathVariable("id") UUID uuid) {
+    public String details(@PathVariable("uuid") UUID uuid) {
         return "details";
     }
 
