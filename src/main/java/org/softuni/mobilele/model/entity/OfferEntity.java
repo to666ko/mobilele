7cdeab9b9872a9ef87dfe7236251dbabb82b126a
Todor Dimitrov <to666ko90@abv.bg>
2023-10-12 23:42:36 +0300

    Bug fix

--- src/main/java/org/softuni/mobilele/model/entity/OfferEntity.java
+++ src/main/java/org/softuni/mobilele/model/entity/OfferEntity.java
@@ -1,8 +1,6 @@
 package org.softuni.mobilele.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.softuni.mobilele.model.enums.EngineEnum;
 import org.softuni.mobilele.model.enums.TransmissionEnum;
@@ -24,8 +22,10 @@ public class OfferEntity extends BaseEntity {
     @ManyToOne
     private ModelEntity model;
 
+    @Enumerated(EnumType.STRING)
     private EngineEnum engine;
 
+    @Enumerated(EnumType.STRING)
     private TransmissionEnum transmission;
 
     private String imageUrl;
